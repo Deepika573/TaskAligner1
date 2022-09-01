6cd32cdf0155c9e00db1412c9303e0f015ca4612
Deepika Rawat <deepika.rawat@hanu.com>
2022-09-01 11:37:01 +0530

    Atul's branch commit

--- Business/DepartmentTeamManager.cs
+++ Business/DepartmentTeamManager.cs
@@ -1,6 +1,36 @@
-﻿namespace TaskAligner.Business
+﻿using TaskAligner.Entities;
+using TaskAligner.Interfaces.Business;
+using TaskAligner.Interfaces.Repository;
+
+namespace TaskAligner.Business
 {
-    public class DepartmentTeamManager
+    public class DepartmentTeamManager : IDepartmentTeamManager
     {
+        private readonly IDepartmentTeamRepository _departmentTeamRepository;
+
+        public DepartmentTeamManager(IDepartmentTeamRepository departmentTeamRepository)
+        {
+            _departmentTeamRepository = departmentTeamRepository;
+        }
+
+        public Task<DepartmentTeam> AddAsync(DepartmentTeam departmentTeam)
+        {
+            return _departmentTeamRepository.AddAsync(departmentTeam);
+        }
+
+        public Task<DepartmentTeam> DeleteAsync(int depTeamid)
+        {
+            return _departmentTeamRepository.DeleteAsync(depTeamid);
+        }
+
+        public Task<IEnumerable<DepartmentTeam>> GetAllDepartmentTeamsAsync()
+        {
+            return _departmentTeamRepository.GetAllDepartmentTeamsAsync();
+        }
+
+        public Task<DepartmentTeam> UpdateAsync(int depTeamid, DepartmentTeam departmentTeam)
+        {
+            return _departmentTeamRepository.UpdateAsync(depTeamid, departmentTeam);
+        }
     }
 }
