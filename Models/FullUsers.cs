a41c24a7e2447dafe9bb013fbd303dc0f17786ea
HarshitSehgal1 <harshit.hanu@outlook.com>
2022-09-02 14:57:25 +0530

    FullUsersModel

---
+++ Models/FullUsers.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskAligner.Models
+{
+    public class FullUsers
+    {
+
+        public string EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string DesignationName { get; set; }
+        public string DepartmentName { get; set; }
+        public string UserName { get; set; }
+        public string Passwords { get; set; }
+        public string Email { get; set; }
+    }
+}
